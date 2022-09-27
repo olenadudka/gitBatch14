bf623fba1ec80539176c7a8b8402faad8935b33d
olenadudka <helen.lady2311@gmail.com>
2022-09-27 19:58:59 -0400

    Third commit

--- GitClass03B14/src/class03B14/class03.java
+++ GitClass03B14/src/class03B14/class03.java
@@ -5,6 +5,7 @@ public class class03 {
 	public static void main(String[] args) {
 		System.out.println("hey this is a git class");
 System.out.println("I made a change in my project");
+System.out.println("One more message");
 	}
 
 }
